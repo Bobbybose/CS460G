0d416c41c6ef36ec6d247d05279fc3eca0bbdc72
Bobby Bose <bobbybose19@gmail.com>
2022-02-09 14:31:30 -0500

    data class

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -19,6 +19,12 @@ class Branch:
     def add_child(self, child):
         self.child = child
 
+class Data:
+    
+    attribute_values = {}
+    class_label = ""
+
+
 def main():
     create_decision_tree()    
     
@@ -27,19 +33,34 @@ def create_decision_tree(dataset, class_label, attributes):
     
     root = Node()
 
-    if num_labels_in_dataset(dataset) == 1:
+    if num_unique_labels_in_dataset(dataset) == 1:
         root.attribute = dataset[0].class_label
         return root
     
     if attributes.size == 0:
         root.attribute = most_common_class_label_value(dataset)
         return root
+    else:
+        root.attribute = best_attribute(attributes)
+    
 
+def num_unique_labels_in_dataset(dataset):
+    num_unique_labels = 0
+    labels = np.array([])
+    for data in dataset:
+        if not data.class_label in labels:
+            num_unique_labels += 1
+            labels.append(data.class_label)            
+    
+    return num_unique_labels
 
-def num_labels_in_dataset(dataset):
-    return 1
 
 def most_common_class_label_value(dataset):
     return 1
+
+
+def best_attribute(attributes):
+    return 1
+
 
 main()
