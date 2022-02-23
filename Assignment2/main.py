9cdd20232756c1c0f0bf7ee042801e95da7adaa6
Bobby Bose <bobbybose19@gmail.com>
2022-02-23 14:43:07 -0500

    Created Assignment2

---
+++ Assignment2/main.py
@@ -0,0 +1,10 @@
+# Author: Bobby Bose
+# Assignment 2: Linear and Polynomial Regression
+
+
+# Imports
+import numpy as np
+import math
+import pandas as pd
+pd.options.display.max_rows = 1000
+import matplotlib.pyplot as plt
