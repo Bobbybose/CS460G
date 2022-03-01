e62a91ec278a05f144adb3765f9f04e7e07e6e22
Bobby Bose <bobbybose19@gmail.com>
2022-03-01 11:58:09 -0500

    Finished Part 1

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -3,96 +3,135 @@
 
 
 # Imports
-from re import A
+from functools import partial
 import numpy as np
 #import math
 import pandas as pd
+
+from Assignment1.main import synthetic_data
 #pd.options.display.max_rows = 1000
 #import matplotlib.pyplot as plt
 
 def main():
+# PART 1 of Assignment----------------------------------------------------------------------------------------------
 
+    # Wine data features
     wine_features = ["fixed acidity", "volatile acidity", "citric acid", "residual sugar", 
                    "chlorides", "free sulfur dioxide", "total sulfur dioxide", "density", 
                    "pH", "sulphates", "alcohol"
                     ]
 
-    wine_data_df = pd.read_csv("datasets/winequality-red.csv", delimiter = ",")#, names = wine_labels)
+    # Reading in wine data
+    wine_data_df = pd.read_csv("datasets/winequality-red.csv", delimiter = ",")
 
-    # Preprocessing input features
-    wine_data_processed = wine_data_df.copy()
-    for feature in wine_features:
-        max = wine_data_processed[feature].max()
-        min = wine_data_processed[feature].min()
+    # Running the linear regression over the wine data
+    wine_weights, wine_MSE = multiple_linear_regression(wine_data_df, wine_features, "quality")
 
-        for index, row in wine_data_processed.iterrows():
-            row[feature] = (int(row[feature]) - min) / (max - min)
+    # Printing the final weights and MSE
+    print("Final Wine Weights: " + str(wine_weights))
+    print("Final Wine MSE: " + str(wine_MSE))
 
-    weights, MSE = multiple_linear_regression(wine_data_processed, wine_features, "quality")
+# PART 2 of Assignment----------------------------------------------------------------------------------------------
 
-    print(weights)
+    synthetic_data_df_1 = pd.read_csv("datasets/synthetic-1.csv", delimiter = ",", names = ["x", "y"])
+    synthetic_data_df_2 = pd.read_csv("datasets/synthetic-2.csv", delimiter = ",", names = ["x", "y"])
 
-    print(MSE)
+    synthetic_dataset_list = [synthetic_data_df_1, synthetic_data_df_2]
+
+    polynomial_values = [2, 3, 5]
+
+    for dataset in synthetic_dataset_list:
+        for poly_value in polynomial_values:
+            weights, MSE = polynomial_regression(dataset, poly_value)
 
 
-    return 1
+
 
 # main()
 
 
+# Description: Runs a linear regression over the dataset
+# Arguments: Dataset of examples, features of the dataset, class label for the dataset
+# Returns: Weights and MSE
 def multiple_linear_regression(dataset, features, class_label):
-    weights = []
-    alpha = 0.001
-    epochs = 100
+    
+    # Randomly initializing the weights
+    weights = np.random.uniform(0, 1, len(features) + 1)
+
+    # Parameters
+    y_values = dataset[class_label].to_numpy()
+    epochs = 750
+    alpha = 0.0001
     MSE = 0
 
-    # Randomly initialize the weights
-    weights = np.random.uniform(0, 1, len(features) + 1).tolist()
 
+    # Adjusting number of times according to epochs values
     for epoch in range(epochs):
 
-        if epoch % 10 == 0:
-            print(weights)
-            print(get_MSE(dataset, weights, class_label))
+        if epoch % 50 == 0:
+            print("Epoch: " + str(epoch))
+            print("     Weights: " + str(weights))
+            print("     MSE: " + str(get_MSE(dataset, weights, y_values)))
 
-        for index in range(len(weights)):
-            gradient_total = 0
-            
-            
-            for i, data in dataset.iterrows():
-                if index == 0:
-                    gradient_total += (get_equation_prediction(weights, data) - data[class_label]) * 1
-                else:
-                    gradient_total += (get_equation_prediction(weights, data) - data[class_label]) * data[index-1]
+        # Updating each weight
+        for weight_index in range(weights.size):
+            weights[weight_index] -= alpha * total_loss(dataset, weights, y_values, weight_index)
 
-            weights[index] -= alpha * 1/(len(dataset)) * gradient_total
+    MSE = get_MSE(dataset, weights, y_values)
     
-    MSE = get_MSE(dataset, weights, class_label)
-
     return weights, MSE
-
 # multiple_linear_regression()
 
 
-def get_MSE(dataset, weights, class_label):
-    MSE = 0
+# Description: Calculates the loss for the regression equation
+# Arguments: Dataset of examples, weights for the equation, y values for the equation, index for the weight/feature being updated
+# Returns: Loss  
+def total_loss(dataset, weights, y_values, weight_index):
     
-    for i, data in dataset.iterrows():
-        MSE += ((get_equation_prediction(weights, data) - data[class_label]))**2
-    MSE *= 1/len(dataset)
+    loss = 0
 
-    return MSE
-
-def get_equation_prediction(weights, data):
-    result = 0
-    for index in range(len(weights)):
+    # Summing the loss from each example
+    for index, data in dataset.iterrows():
+        # Retrieving and cleaning up the x_values for the equation 
+        x_values = data.to_numpy()
+        x_values = np.delete(x_values, -1)
+        x_values = np.insert(x_values, 0, 1)
         
-        if index == 0:
-            result += weights[index]*1
-        else:
-            result += weights[index]*data[index-1]
+        # Calculating the loss for this example
+        loss += (np.sum(np.multiply(weights, x_values)) - y_values[index]) * x_values[weight_index]
+
+    return (1/len(y_values)) * loss
+# total_loss()
+
+
+# Description: Calculates and returns the MSE
+# Arguments: Dataset of examples, weights for the equation, y values for the equation
+# Returns: MSE
+def get_MSE(dataset, weights, y_values):
+
+    loss = 0
+
+    # Summing the loss from each example
+    for index, data in dataset.iterrows():
+        # Retrieving and cleaning up the x_values for the equation 
+        x_values = data.to_numpy()
+        x_values = np.delete(x_values, -1)
+        x_values = np.insert(x_values, 0, 1)
+
+        # Calculating the loss for this example
+        loss += (np.sum(np.multiply(weights, x_values)) - y_values[index]) ** 2
+
+    # Returning the full MSE
+    return (1/len(y_values)) * loss 
+# get_MSE
+
+
+def polynomial_regression(dataset, n):
+    # Randomly initializing the weights
+    weights = np.random.uniform(0, 1, n+1)
     
-    return result
-# get_equation_prediction()
+    # x and y values of the dataset
+    x_values = dataset["x"].to_numpy()
+    y_values = dataset["y"].to_numpy()
 
 main()
