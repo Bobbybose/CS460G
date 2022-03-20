ea3d5a1b0c866bffeac28a06dd03f9460d4c5cc6
Bobby Bose <bobbybose19@gmail.com>
2022-03-20 17:30:04 -0400

    Started working on backprop

--- Assignment3/main.py
+++ Assignment3/main.py
@@ -40,23 +40,57 @@ def main():
     # 1x1
     bias_o = np.random.uniform(0, 1, num_nodes_in_layer[2]).tolist()
 
-    print(bias_h)
-    print(bias_o)
-
     alpha = 0.5
     epochs = 1
 
     for epoch in range(epochs):
         for index, data in enumerate(training_data_x):
             # data is [784 x 1]
-            forward_pass(weights_h, weights_o, bias_h, bias_o, data)
+            weights_h, weights_o, bias_h, bias_o = backpropagation(alpha, weights_h, weights_o, bias_h, bias_o, data, training_data_y[index])
+            
+
+
+def backpropagation(alpha, weights_h, weights_o, bias_h, bias_o, x, y):
+    
+    # Values needed for updating
+    in_h, h, in_o, o = forward_pass(weights_h, weights_o, bias_h, bias_o, x)
+    error = y - o
+    # 1x1
+    delta_o = error * sigmoid_prime(in_o)
 
 
 
+    # 50x1
+    delta_h = np.dot(np.dot(weights_o, delta_o), list(map(sigmoid_prime, in_h)))
 
+
+
+    # Updating weights and biases
+    weights_o = weights_o + alpha * np.dot(h, delta_o)
+    bias_o = bias_o + np.array(alpha * delta_o)
+    weights_h = weights_h + alpha * np.outer(x, np.array(delta_h).transpose())
+    bias_h = bias_h + alpha * delta_h
+
+    return weights_h, weights_o, bias_h, bias_o
+
+
+# Description: Calculates the forward pass of the neural net
+# Arguments: weights and biases of neural net, input
+# Returns: Neural Net prediction
 def forward_pass(weights_h, weights_o, bias_h, bias_o, x):
     # 50x1
     in_h = np.dot(np.array(weights_h).transpose(), x) + bias_h
+
+    # 50x1
+    h = list(map(sigmoid, in_h))
+
+    # 1x1
+    in_o = np.dot(np.array(weights_o).transpose(), h) + bias_o
+
+    # 1x1
+    o = sigmoid(in_o)
+
+    return in_h, h, in_o, o
 
 
 # Description: Calculates sigmoid function result of x
