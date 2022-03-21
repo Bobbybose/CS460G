f035e2bacdf67254f370f89db0d5fe27cbc8e2c2
Bobby Bose <bobbybose19@gmail.com>
2022-03-21 14:54:05 -0400

    Everything seems to be working

--- Assignment3/main.py
+++ Assignment3/main.py
@@ -16,9 +16,9 @@ def main():
     test_dataset_df = pd.read_csv("datasets/mnist_test_0_1.csv")
     
     training_data_x = list(map(lambda x: x/255, training_dataset_df.iloc[:, 1:].to_numpy()))
-    training_data_y = training_dataset_df.iloc[:, 0].tolist()
+    training_data_y = list(training_dataset_df.iloc[:, 0])
     test_data_x = list(map(lambda x: x/255, test_dataset_df.iloc[:, 1:].to_numpy()))
-    test_data_y = test_dataset_df.iloc[:, 0].tolist()
+    test_data_y = list(test_dataset_df.iloc[:, 0])
 
     # 784 Input features
     # Structure: 784 -> 50 -> 1
@@ -28,28 +28,54 @@ def main():
     weights_h = []
     # 50x1
     weights_o = []
-
+    
+    # Initializing weights
     for i in range(num_nodes_in_layer[0]):
-        weights_h.append(np.random.uniform(-1, 1, num_nodes_in_layer[1]).tolist())
+        weights_h.append(list(np.random.uniform(-1, 1, num_nodes_in_layer[1])))
 
     for i in range(num_nodes_in_layer[1]):
-        weights_o.append(np.random.uniform(-1, 1, num_nodes_in_layer[2]).tolist())
+        weights_o.append(np.random.uniform(-1, 1))
 
     # 50x1
     bias_h = np.random.uniform(0, 1, num_nodes_in_layer[1]).tolist()
     # 1x1
     bias_o = np.random.uniform(0, 1, num_nodes_in_layer[2]).tolist()
 
+    # Parameters for neural net
     alpha = 0.5
-    epochs = 1
+    epochs = 500
 
+    # Training neural net
     for epoch in range(epochs):
-        for index, data in enumerate(training_data_x):
+        for index, x in enumerate(training_data_x):
             # data is [784 x 1]
-            weights_h, weights_o, bias_h, bias_o = backpropagation(alpha, weights_h, weights_o, bias_h, bias_o, data, training_data_y[index])
+            weights_h, weights_o, bias_h, bias_o = backpropagation(alpha, weights_h, weights_o, bias_h, bias_o, x, training_data_y[index])
             
 
+    # Track how many predictions are correct
+    num_correct = 0
+    # Testing neural net
+    for index, x in enumerate(test_data_x):
+        in_h, h, in_o, prediction = forward_pass(weights_h, weights_o, bias_h, bias_o, x)
 
+        # Rounding the prediction up or down to the class label
+        if prediction >= 0.5:
+            prediction = 1
+        else:
+            prediction = 0
+
+        # Tallying up correct predictions
+        if prediction == test_data_y[index]:
+            num_correct += 1
+
+    # Printing the neural net accuracy
+    print("Num correct: ", num_correct)
+    print("Neural Net accuracy:", num_correct/len(test_data_y))
+
+
+# Description: Runs backpropagation through the neural network and updates weights
+# Arguments: learning rate, weights and biases of neural net, input, and correct class label
+# Returns: Updated weights and biases
 def backpropagation(alpha, weights_h, weights_o, bias_h, bias_o, x, y):
     
     # Values needed for updating
@@ -58,12 +84,8 @@ def backpropagation(alpha, weights_h, weights_o, bias_h, bias_o, x, y):
     # 1x1
     delta_o = error * sigmoid_prime(in_o)
 
-
-
     # 50x1
-    delta_h = np.dot(np.dot(weights_o, delta_o), list(map(sigmoid_prime, in_h)))
-
-
+    delta_h = np.multiply( np.dot(weights_o, delta_o), list(map(sigmoid_prime, in_h)))
 
     # Updating weights and biases
     weights_o = weights_o + alpha * np.dot(h, delta_o)
